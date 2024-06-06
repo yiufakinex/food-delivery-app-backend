194b506f4a17bd9c18382ed727f1f9810ad7f627
Fa Kin Yiu, Franklin <lifeaskrystal@hotmail.com>
2024-06-05 22:26:21 -0600

    Added Restaurant Search Endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoutes";
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(()=> console.log("Connected to database!"));
@@ -30,6 +31,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7000, ()=>{
     console.log("Server started on localhost:7000");
